f30fabc35464b1ddb762d2ab1fa5cfd599cf16cd
nekobato <nekobato@gmail.com>
2018-03-08 16:01:37 +0900

    flexible port at client

--- src/lib/websocket.js
+++ src/lib/websocket.js
@@ -3,7 +3,7 @@ const io = require('socket.io-client')
 export default {
   socket: null,
   start() {
-    this.socket = io('ws://localhost:3000')
+    this.socket = io(process.env.NODE_ENV === 'production' ? 'ws://palette.nekobato.net/ws' : 'ws://localhost:3000')
 
     this.socket.on('connect', () => {
       this.socket.emit('change', { tool: 'pen', prop: 'color', value: '#999' })
